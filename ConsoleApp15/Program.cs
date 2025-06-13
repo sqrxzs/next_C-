48891084c7413701f9291ed0a017c44a6870b893
sqrxzs <broyanneta@gmail.com>
2025-06-13 17:50:30 +0800

    Встреча №7

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+
+        string text = @"Вот дом,Который построил Джек.А это пшеница,Которая в темном чулане хранится В доме,Который построил 
+Джек. А это веселая птица-синица,Которая часто ворует пшеницу, Которая в темном чулане хранитится В доме Который построил Джек
+Который построил Джек.";
+
+
+        string cleanedText = Regex.Replace(text, @"[^\w\s]", " ");
+        cleanedText = cleanedText.ToLower();
+
+ 
+        string[] words = cleanedText.Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+        
+        Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+        foreach (var word in words)
+        {
+            if (wordCount.ContainsKey(word))
+                wordCount[word]++;
+            else
+                wordCount[word] = 1;
+        }
+        Console.WriteLine("{0,-20} | {1,5}", "Слово", "Кол-во");
+        Console.WriteLine(new string('-', 28));
+        foreach (var pair in wordCount)
+        {
+            Console.WriteLine("{0,-20} | {1,5}", pair.Key, pair.Value);
+        }
+    }
+}
